4b5866c39612bec7a7cf9ff00f84efd2b1b4b147
桂电甜甜 <sxxgryx27@163.com>
2022-05-27 16:54:35 +0800

    2022.05.27
    version：2.4.2
    修复登录界面漏洞

--- pages/login/login.js
+++ pages/login/login.js
@@ -17,25 +17,60 @@ Page({
   //获取微信用户信息
   getUserProfile() {
     that = this
+    that.setData({
+      openid: app.globalData.openid
+    })
     console.log('点击获取用户信息')
-    wx.getUserProfile({
-      desc: '用于完善个人信息',
-      success: (res) => {
-        let user = res.userInfo
-        console.log('获取到的用户信息:', user)
-        app.globalData.userInfo = user
-        console.log("全局用户信息", app.globalData.userInfo)
-        // that.addUserInfo(user.nickName, user.avatarUrl)
-        app.globalData.userName = user.nickName
-        app.globalData.portrait = user.avatarUrl
-        let times = 1;
-        // console.log("全局用户姓名", app.globalData.userName) 
-        that.addUserInfo(user.nickName, user.avatarUrl, times)
-        console.log("云函数调用成功", res)
-        wx.switchTab({
-          url: '../index/index'
-        });
+    //云函数的调用
+    wx.cloud.callFunction({
+      name: "selectUserInfo",
+      data: {
+        _openid: app.globalData.openid
       },
+      success(res) {
+        // console.log('数据类型：',typeof app.globalData.openid)
+        wx.showToast({
+          title: '你来过呀！！！',
+        })
+        //result 用户数据
+        console.log('用户数据', res.result)
+        let result = res.result.data[0];
+        if (result != null) {
+          // console.log(result.userName,result.portrait)
+          app.globalData.userName = result.userName
+          app.globalData.portrait = result.portrait
+          //查到了 更新一下吧
+          app.globalData.times = result.times + 1;
+          that.updateUserInfo(app.globalData.userName, app.globalData.portrait, app.globalData.times)
+          console.log("云函数调用成功", res)
+          wx.switchTab({
+            url: '../index/index'
+          });
+        } else {
+          wx.getUserProfile({
+            desc: '用于完善个人信息',
+            success: (res) => {
+              let user = res.userInfo
+              console.log('获取到的用户信息:', user)
+              app.globalData.userInfo = user
+              console.log("全局用户信息", app.globalData.userInfo)
+              // that.addUserInfo(user.nickName, user.avatarUrl)
+              app.globalData.userName = user.nickName
+              app.globalData.portrait = user.avatarUrl
+              let times = 1;
+              // console.log("全局用户姓名", app.globalData.userName) 
+              that.addUserInfo(user.nickName, user.avatarUrl, times)
+              console.log("云函数调用成功", res)
+              wx.switchTab({
+                url: '../index/index'
+              });
+            },
+          })
+        }
+      },
+      fail(err) {
+        console.error("云函数调用失败", err);
+      }
     })
   },
   //通过openid查找用户信息
